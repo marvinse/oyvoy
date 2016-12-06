10d69f3fd17a7e9913a7a9b57198b51f34f9c385
Marvin Solano <marsolan@crsjopgp4311.global.publicisgroupe.net>
2016-12-05 22:07:25 -0600

    Adding events to the menu

--- dev/components/menu/menu.js
+++ dev/components/menu/menu.js
@@ -11,8 +11,25 @@ APP.menu = (function () {
     var bindEventsToUI = function(){
     	$('.menu__item span').click(function(){
     		$(this).toggleClass('is-active');
-    		$(this).parent('li').find('.submenu').toggle();
+            var parent = $(this).parent('li');
+    		parent.find('.submenu').toggle();
+            parent.siblings('li').find('.submenu').hide();
+            parent.siblings('li').find('>span').removeClass('is-active');
+            if( $(this).hasClass('is-active') ){
+                $('.popup').show();
+            }else{
+                $('.popup').hide();
+            }
     	});
+        $('.menu__item--calendar .submenu__items a').click(function(e){
+            e.preventDefault();
+            var index = $(this).index();
+            $(this).addClass('is-active');
+            $(this).siblings().removeClass('is-active');
+            var newListActive = $(this).parents('.submenu').find('>li').eq(index);
+            newListActive.siblings('li').hide();
+            newListActive.show();
+        });
     };
 
     return {
